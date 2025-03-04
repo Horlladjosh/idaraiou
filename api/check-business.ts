80168c14f6f77cc7825f1292bde6c7b8ce54ac31
Joshua <oladimeji.adegbite03@gmail.com>
2025-03-04 18:12:37 +0100

    add Vercel type

--- api/check-business.ts
+++ api/check-business.ts
@@ -1,6 +1,7 @@
 import fetch from 'node-fetch';
+import type { VercelRequest, VercelResponse } from '@vercel/node'
 
-export default async function handler(req: any, res: any) {
+export default async function handler(req: VercelRequest, res: VercelResponse) {
   // CORS Configuration
   const ALLOWED_ORIGIN = '*';
 
